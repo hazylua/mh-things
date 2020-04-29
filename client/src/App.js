2aef9e6cba456e945e3b428ee9bfeab648354fd9
yb00 <40061395+yb00@users.noreply.github.com>
2020-04-28 21:22:18 -0300

    Add backend

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import FilterTable from './pages/FilterTable'
+import ArmorSetSearch from './pages/ArmorSetSearch'
+
+import './App.css'
+
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li><Link className="navbar-link" to="/mh-things/">Home</Link></li>
+      <li><Link className="navbar-link" to="/mh-things/skill-filter">Filter Page</Link></li>
+      <li><Link className="navbar-link" to="/mh-things/ass">Armor Set Search</Link></li>
+      <li><Link className="navbar-link" to="/mh-things/about">About</Link></li>
+    </ul>
+  </nav>
+)
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/mh-things" component={Home} />
+      <Route path="/mh-things/about" component={About} />
+      <Route path="/mh-things/skill-filter" component={FilterTable} />
+      <Route path="/mh-things/ass" component={ArmorSetSearch} />
+    </Switch>
+  </main>
+)
+
+const Home = () => (
+  <div>
+    <h1 className="page-title">Home Page</h1>
+    <p>Elit ullamco id labore fugiat esse mollit deserunt culpa nisi adipisicing irure reprehenderit ipsum.</p>
+    <p>Ex ut exercitation quis id eiusmod cillum deserunt culpa proident exercitation fugiat aliquip Lorem. Ipsum aliqua in tempor irure laboris reprehenderit veniam nostrud pariatur ex eiusmod labore deserunt nisi. Nulla enim consequat pariatur occaecat ullamco nostrud voluptate est anim voluptate. Voluptate reprehenderit labore et proident id et mollit irure ut. Dolore ea quis aute tempor ad nostrud ut cillum anim tempor aute reprehenderit. Ea eu sint est fugiat laborum cillum exercitation dolor non eu quis minim minim deserunt. Fugiat aliquip dolor culpa ut mollit mollit enim.</p>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h1 className="page-title">About</h1>
+
+    <p>Elit ullamco id labore fugiat esse mollit deserunt culpa nisi adipisicing irure reprehenderit ipsum.</p>
+    <p>Ex ut exercitation quis id eiusmod cillum deserunt culpa proident exercitation fugiat aliquip Lorem. Ipsum aliqua in tempor irure laboris reprehenderit veniam nostrud pariatur ex eiusmod labore deserunt nisi. Nulla enim consequat pariatur occaecat ullamco nostrud voluptate est anim voluptate. Voluptate reprehenderit labore et proident id et mollit irure ut. Dolore ea quis aute tempor ad nostrud ut cillum anim tempor aute reprehenderit. Ea eu sint est fugiat laborum cillum exercitation dolor non eu quis minim minim deserunt. Fugiat aliquip dolor culpa ut mollit mollit enim.</p>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Main />
+  </Router>
+)
+
+export default App;
---
+++ client/src/pages/FilterTable.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+class FilterTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rowData: [],
+      filtered: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/yb00/mh-things/source/json/skill_pages.json"
+    )
+      .then(response => response.json())
+      .then(json => {
+        let rows = [];
+        for (let object of json) {
+          let row_num = 1;
+          for (let skill of object.decos) {
+            rows.push({
+              skill: skill,
+              page: object.page,
+              row: row_num
+            });
+            row_num += 1;
+          }
+        }
+
+        this.setState({
+          rowData: rows,
+          filtered: rows
+        });
+      });
+  }
+
+  onSkillInputTextChange(e) {
+    this.setState({
+      filtered: this.state.rowData.filter(
+        skill =>
+          skill.skill.toUpperCase().indexOf(e.target.value.toUpperCase()) !== -1
+      )
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="page-title">Filter Table</h1>
+        <input
+          id="skill-input"
+          type="text"
+          className="skill-input"
+          placeholder="Skill Name"
+          onChange={this.onSkillInputTextChange.bind(this)}
+        />
+        <table className="skill-table">
+          <thead>
+            <tr>
+              <th style={{ width: "60%" }}>Skill Name</th>
+              <th style={{ width: "20%" }}>Page Number</th>
+              <th style={{ width: "20%" }}>Row Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.filtered.map(skill => {
+              return (
+                <tr>
+                  <td>{skill.skill}</td>
+                  <td>{skill.page}</td>
+                  <td>{skill.row}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default FilterTable;
