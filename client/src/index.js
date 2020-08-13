1737c843412f467dea11bac50c312d852d1e75ab
yb00 <yagobraz98@gmail.com>
2020-08-13 10:20:53 -0300

    Added bootstrap reqs to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// Scripts
+import "jquery/dist/jquery.js";
+import "popper.js/dist/umd/popper.js";
+import "bootstrap/dist/js/bootstrap.js";
+
+import "./index.css";
+
+import App from "./App";
 
 ReactDOM.render(
-
-  < React.StrictMode >
+  <React.StrictMode>
     <App />
-  </React.StrictMode >,
-  document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
