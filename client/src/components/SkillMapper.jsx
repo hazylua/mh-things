069c09523d4e67d721de5c79fcfed85c42eb6e48
yb00 <yagobraz98@gmail.com>
2020-08-16 01:10:28 -0300

    New component

---
+++ client/src/components/SkillMapper.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import { Container, Col } from "react-bootstrap";
+
+const SkillMapper = () => {
+  return <React.Fragment></React.Fragment>;
+};
+
+export default SkillMapper;
