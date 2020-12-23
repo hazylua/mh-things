2678dafe0fa5a6c32fee005a3163dba1c18e41b6
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-22 22:34:46 -0300

    Update app

--- src/app/App.js
+++ src/app/App.js
@@ -1,114 +1,11 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from "react-router-dom";
-
-import HomePage from "../pages/HomePage"
-import AboutPage from "../pages/AboutPage"
-import FilterTable from "../pages/FilterTablePage";
-import ArmorSetSearcher from "../pages/SetSearcherPage";
-import LoginPage from "../pages/LoginPage"
-import RegisterPage from "../pages/RegisterPage"
-
+import { BrowserRouter as Router } from "react-router-dom";
+import RouteConfig from "../routes";
 import "./App.css";
-
-const Navbar = () => (
-  <div className="header">
-    <div className="header-title">
-      <NavLink
-        style={{
-          display: "flex",
-          alignItems: "center",
-          textDecoration: "none",
-        }}
-        to="/"
-      >
-        <h1>MH_THINGS</h1>
-      </NavLink>
-    </div>
-    <nav className="header-nav">
-      <ul>
-        <li>
-          <NavLink
-            className="navbar-link"
-            activeClassName="navbar-link-active"
-            exact
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navbar-link"
-            activeClassName="navbar-link-active"
-            to="/skill-filter"
-          >
-            Filter Page
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navbar-link"
-            activeClassName="navbar-link-active"
-            to="/ass"
-          >
-            Armor Set Searcher
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navbar-link"
-            activeClassName="navbar-link-active"
-            to="/register"
-          >
-            Register
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navbar-link"
-            activeClassName="navbar-link-active"
-            to="/login"
-          >
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navbar-link"
-            activeClassName="navbar-link-active"
-            to="/about"
-          >
-            About
-          </NavLink>
-        </li>
-
-      </ul>
-    </nav>
-  </div>
-);
-
-const Main = () => (
-  <div>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/skill-filter" component={FilterTable} />
-      <Route path="/ass" component={ArmorSetSearcher} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/register" component={RegisterPage} />
-      <Route path="/about" component={AboutPage} />
-    </Switch>
-  </div>
-);
 
 const App = () => (
   <Router>
-    <Navbar />
-    <Main />
+    <RouteConfig></RouteConfig>
   </Router>
 );
 
