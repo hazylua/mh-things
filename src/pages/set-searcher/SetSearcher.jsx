63ae556e4a1b695dd3be819580617485b48b6fe6
yb00 <yagobraz98@gmail.com>
2020-12-25 23:30:01 -0300

    Format set searcher page

--- src/pages/set-searcher/SetSearcher.jsx
+++ src/pages/set-searcher/SetSearcher.jsx
@@ -4,79 +4,92 @@ import "./SetSearcher.css";
 
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
+import "ag-grid-community/dist/styles/ag-theme-fresh.css";
 
-import { Navbar } from "../../components/Navbar";
+import { Layout } from "../../components/Layout";
+
+import { mhwdb } from "../../services";
 
 const onFirstDataRendered = (params) => {
   params.api.sizeColumnsToFit();
 };
 
-const SetSearcher = () => {
-  const [skills, skillsAdd] = useState([]);
-  const [setSkills, setSkillsAdd] = useState([]);
-  const [filtered, addFiltered] = useState([]);
+const SetSkillsCols = [
+  {
+    headerName: "Skill Name",
+    field: "name",
+    filter: true,
+  },
+];
+const ChosenSkillsCols = [
+  {
+    headerName: "Skill Chosen",
+    field: "name",
+  },
+  {
+    headerName: "Skill Level",
+    field: "ranks.length",
+    editable: true,
+  },
+];
 
-  const colDefs = [
-    {
-      headerName: "Skill Name",
-      field: "name",
-    },
-  ];
-  const colSets = [
-    {
-      headerName: "Skill Chosen",
-      field: "name",
-    },
-    {
-      headerName: "Skill Level",
-      field: "ranks.length",
-      editable: "true",
-    },
-  ];
+const SetSearcher = () => {
+  const [skills, skillsSet] = useState([]);
+  const [skillsChosen, skillsChosenSet] = useState([]);
+
+  const handleChosenSkillsRemove = (value) => {
+    skillsChosenSet(skillsChosen.filter((skill) => skill !== value));
+  };
 
   useEffect(() => {
-    let isSubscribed = true;
+    const handleFetch = async () => {
+      try {
+        const response = await mhwdb().get("/skills");
+        const data = await response.data;
+        skillsSet(data);
+      } catch (err) {}
+    };
 
-    fetch("https://mhw-db.com/skills")
-      .then((response) => response.json())
-      .then((json) => {
-        if (isSubscribed) {
-          skillsAdd(json);
-          addFiltered(json);
-        } else {
-          return null;
-        }
-      });
-    return () => (isSubscribed = false);
+    handleFetch();
   }, []);
 
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Layout>
+      <div className="container">
+        <div className="info-section">
+          <p>Search for any sets you want here.</p>
+        </div>
+        <div className="set-searcher-area">
+          {/* <aside className="set-searcher-settings"> + </aside> */}
+          <div className="ag-theme-fresh set-skills-table">
+            <AgGridReact
+              onFirstDataRendered={onFirstDataRendered}
+              columnDefs={SetSkillsCols}
+              rowData={skills}
+              onCellClicked={(params) => {
+                if (skillsChosen.indexOf(params.node.data) === -1) {
+                  skillsChosenSet([...skillsChosen, params.node.data]);
+                }
+              }}
+            ></AgGridReact>
+          </div>
+          <span className="sep" />
+          <div className="ag-theme-fresh results-table">
+            <AgGridReact
+              onFirstDataRendered={onFirstDataRendered}
+              columnDefs={ChosenSkillsCols}
+              rowData={skillsChosen}
+              onCellClicked={(params) =>
+                handleChosenSkillsRemove(params.node.data)
+              }
+            ></AgGridReact>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
-/* <div className="ag-theme-alpine-dark">
-  <AgGridReact
-    onFirstDataRendered={onFirstDataRendered}
-    columnDefs={colDefs}
-    rowData={filtered}
-    onCellClicked={(params) => {
-      if (setSkills.indexOf(params.node.data) === -1) {
-        setSkillsAdd((setSkills) => [...setSkills, params.node.data]);
-      }
-    }}
-  ></AgGridReact>
-</div>; */
-
-/* <div className="ag-theme-alpine-dark">
-  <AgGridReact
-    onFirstDataRendered={onFirstDataRendered}
-    columnDefs={colSets}
-    rowData={setSkills}
-  ></AgGridReact>
-</div>; */
+/* ; */
 
 export default SetSearcher;
