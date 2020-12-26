9580755f78aac6595e15b1cbe3b1681659340139
yb00 <yagobraz98@gmail.com>
2020-12-25 23:30:29 -0300

    Add mhwdb axios connection

---
+++ src/services/external/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default () => {
+    return axios.create({
+        baseURL: "https://mhw-db.com",
+        timeout: 10000,
+    });
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,2 @@
 export { default as api } from "./api";
+export { default as mhwdb } from "./external"
