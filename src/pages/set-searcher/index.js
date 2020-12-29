ea601eb38b2268f2842b63e54ae5b1182f3df32a
yb00 <yagobraz98@gmail.com>
2020-12-28 22:29:23 -0300

    Export components

--- src/pages/set-searcher/index.js
+++ src/pages/set-searcher/index.js
@@ -1,1 +1,4 @@
 export { default as SetSearcher } from "./SetSearcher";
+export { SkillsTable } from "./SkillsTable";
+export { PickedTable } from "./PickedTable";
+export { SettingsBar } from "./SettingsBar"
