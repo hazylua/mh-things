2e824ed244872fb273944b68ebd46def510c13ee
yb00 <yagobraz98@gmail.com>
2020-12-29 23:22:08 -0300

    Remove node on shift click

--- src/pages/set-searcher/PickedTable.jsx
+++ src/pages/set-searcher/PickedTable.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { AgGridReact } from "ag-grid-react";
+import { AgGridReact, AgGridColumn } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-fresh.css";
 
@@ -26,15 +26,37 @@ export const PickedTable = ({ skillsChosen, setSkillsChosen }) => {
     setSkillsChosen(skillsChosen.filter((skill) => skill.id !== id));
   };
 
-
   return (
     <div className="ag-theme-fresh results-table">
       <AgGridReact
         onFirstDataRendered={onFirstDataRendered}
-        columnDefs={ChosenSkillsCols}
         rowData={skillsChosen}
-        onCellClicked={(params) => removeSkillsChosen(params.node.data)}
-      ></AgGridReact>
+        // Remove node by holding shift and clicking it.
+        onCellClicked={(params) => {
+          if (params.event.shiftKey) removeSkillById(params.node.data.id);
+        }}
+      >
+        <AgGridColumn headerName="Skill Name" field="name" />
+        <AgGridColumn
+          headerName="Amount"
+          field="amount"
+          editable={true}
+          resizable={true}
+          valueGetter={(params) => {
+            return params.data.amount;
+          }}
+          // Checks to see if amount picked for skill is valid.
+          valueSetter={(params) => {
+            const newAmount = params.newValue;
+            const ranksLength = params.data.ranks.length;
+            if (newAmount < 0 || newAmount > ranksLength) return false;
+            else {
+              params.data.amount = newAmount;
+              return true;
+            }
+          }}
+        />
+      </AgGridReact>
     </div>
   );
 };
