fa746cd70642b4f9b61b0e3feb51161c73cb3d0b
yb00 <yagobraz98@gmail.com>
2021-01-04 22:32:39 -0300

    Update charm map

--- src/pages/set-searcher/actions/map.js
+++ src/pages/set-searcher/actions/map.js
@@ -21,11 +21,28 @@ export function mapSkills(skills, armorDB, charmsDB, decorationsDB) {
     }
   }
 
-  for(var charm of charmsDB) {
-    const obj = {
-      id: charm.id,
-      slots: null,
-      name: charm.name,
+  // Map charms.
+  for (var charm of charmsDB) {
+    for (var rank of charm["ranks"]) {
+      for (var skill of rank["skills"]) {
+        const skill_name = skill.skillName;
+        const obj = {
+          id: charm.id,
+          type: "charm",
+          rarity: rank.rarity,
+          origin: charm.name,
+          name: rank.name,
+          skill_amount: skill.level,
+        };
+
+        // Check if key exists. If not, add empty object to it. Same for subkey.
+        if (!skillMap[skill_name]) skillMap[skill_name] = {};
+        if (!skillMap[skill_name]["charms"])
+          skillMap[skill_name]["charms"] = [];
+        skillMap[skill_name]["charms"].push(obj);
+      }
+    }
+  }
 
     }
   }
