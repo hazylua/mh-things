5e4bb28810eb2d501306aab15fea04f88c87952b
yb00 <yagobraz98@gmail.com>
2021-01-05 21:21:36 -0300

    Add subset functions

---
+++ src/pages/set-searcher/actions/subset.js
@@ -0,0 +1,125 @@
+import _ from "lodash";
+
+const sortDescendingSkillAmount = (inventory) => {
+  return inventory
+    .sort((a, b) =>
+      a.skill_amount > b.skill_amount
+        ? 1
+        : b.skill_amount > a.skill_amount
+        ? -1
+        : 0
+    )
+    .reverse();
+};
+
+export const find = (skillsChosen, skillMap, charmMap, armorMap, config) => {
+  const maxResults = 10;
+  var inventory = {
+    head: [],
+    chest: [],
+    gloves: [],
+    waist: [],
+    legs: [],
+    charm: [],
+    decorations: [null, [], [], [], []],
+  };
+
+  for (var skill of skillsChosen) {
+    const skill_name = skill.name;
+    const skill_amount = skill.amount;
+    const skill_id = skill.id;
+    // Only master for now. Will decide based on 'config' settings later.
+    // Check if skill is in.
+    if (skillMap[skill_name] && skillMap[skill_name]["master"])
+      for (var piece of skillMap[skill_name]["master"]) {
+        inventory[piece.type].push({ ...piece, skill_name: skill_name });
+      }
+    // Check if skill is in.
+    if (skillMap[skill_name]["charms"])
+      for (var charm of skillMap[skill_name]["charms"]) {
+        inventory["charm"].push({ ...charm, skill_name: skill_name });
+      }
+    // Check if skill is in.
+    if (skillMap[skill_name]["decorations"])
+      for (var [idx, decorations] of skillMap[skill_name][
+        "decorations"
+      ].entries()) {
+        if (idx)
+          for (var decoration of decorations) {
+            inventory["decorations"][idx].push({
+              ...decoration,
+              skill_name: skill_name,
+            });
+          }
+      }
+  }
+
+  for (var i in inventory) {
+    if (i === "decorations")
+      for (var j = 1; j <= 4; j++) {
+        inventory[i][j] = sortDescendingSkillAmount(inventory[i][j]);
+      }
+    else {
+      inventory[i] = sortDescendingSkillAmount(inventory[i]);
+    }
+  }
+  console.log(inventory);
+
+  // Add stop conditions (empty objects).
+  for (var i in inventory) {
+    if (i === "decorations")
+      for (var j = 1; j <= 4; j++) {
+        inventory[i][j].push({});
+      }
+    else {
+      inventory[i].push({});
+    }
+  }
+
+  var sets = [];
+
+  // Fit head.
+  for (var headPiece of inventory["head"]) {
+    var armor = { decorations: [] };
+    var skills = { slots: [null, 0, 0, 0, 0], skills: {} };
+    var { t, c, a } = fit(
+      armor,
+      headPiece,
+      headPiece.type,
+      skills,
+      charmMap,
+      armorMap
+    );
+  }
+};
+
+const chest = () => {};
+const gloves = () => {};
+const waist = () => {};
+const legs = () => {};
+const charm = () => {};
+
+const fit = (set, piece, pieceType, total, charmMap, armorMap) => {
+  if (_.isEmpty(piece)) return [total, true, set];
+
+  var picked = null;
+  // Get last upgrade of charm from charMap, which is charmMap[id][length-1]
+  if (pieceType === "charm") {
+    const highest = charmMap[piece.id].length - 1;
+    const id = piece.id;
+    picked = charmMap[id][highest];
+  } else {
+    const id = piece.id;
+    picked = armorMap[id];
+  }
+
+  // var set_copy = _.cloneDeep(set)
+  // set_copy[pieceType] = piece.id
+
+  // total_copy = _.cloneDeep(total)
+
+  // for(var slot of piece.slots)
+  //   total_copy['slots'][slot] += 1
+  
+  
+};
